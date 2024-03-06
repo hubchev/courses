3184d7745bb0943c69258efce5dd4896450932a9
hubchev <DrStephanHuber@yahoo.com>
2024-03-06 15:13:12 +0100

    u1

--- scr/exe_import_covid.R
+++ scr/exe_import_covid.R
@@ -2,7 +2,7 @@
 
 # load packages
 if (!require(pacman)) install.packages("pacman")
-pacman::p_load(tidyverse)
+pacman::p_load(tibble)
 
 state <- c("BY", "NRW", "BW")
 deaths <- c(4.92, 5.32, 3.69)
